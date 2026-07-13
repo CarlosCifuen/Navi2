c603e6755242929164dfae10b09003f3b159a530
CarlosCifuen <cif24196@uvg.edu.gt>
2026-07-13 10:52:28 -0600

    app base que compila y muestra un primer Composable en el emulador.

--- app/src/main/java/com/proyecto/navi/MainActivity.kt
+++ app/src/main/java/com/proyecto/navi/MainActivity.kt
@@ -32,7 +32,7 @@ fun WelcomeScreen() {
         ) {
             Text("\uD80C\uDC83 ࣪˖ Navi ִֶָ\uD800\uDC14٠ ࣪⭑", style = MaterialTheme.typography.headlineMedium)
             Spacer(modifier = Modifier.height(8.dp))
-            Text("Organiza tus tareas y mantén tu racha")
+            Text("¡Organiza tus tareas y mantén tu racha!")
         }
     }
 }
