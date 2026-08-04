055a0cad02f575914861e857829f795b410e5b23
CarlosCifuen <cif24196@uvg.edu.gt>
2026-08-04 17:47:07 -0600

    Se creo la pantalla de Ajustes "SettingsScreen"
    Implementa la pantalla de Ajustes en Jetpack Compose, con carga de imagen meduante Coil y componentes reutilizables como SettingsItem y NotificationToggleItem.

--- app/src/main/java/com/proyecto/navi/MainActivity.kt
+++ app/src/main/java/com/proyecto/navi/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                WelcomeScreen()
+                SettingsScreen()
             }
         }
     }
